05b68af09518af137b284a9e5ccdde08fd7c84f9
NozomiS0129 <129555124+NozomiS0129@users.noreply.github.com>
2024-04-10 00:01:13 -0400

    create navbar

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  constructor(
+    public router: Router
+  ) { }
+
+  ngOnInit(): void { }
+
+  public openMovieList(): void {
+    this.router.navigate(['movies']);
+  }
+
+  public openProfile(): void {
+    this.router.navigate(['profile']);
+  }
+
+  public logoutUser(): void {
+    localStorage.setItem('token', '');
+    localStorage.setItem('user', '');
+    this.router.navigate(['welcome']);
+  }
+}
